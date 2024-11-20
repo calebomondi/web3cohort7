3d34533bf0367db5be5211a1d88d4a88c0c26f2d
calebomondi <caleb.omondi@students.jkuat.ac.ke>
2024-11-20 21:52:49 +0300

    added logic to the inheriting class AdvancedBookstore

--- contracts/AdvancedBookstore.sol
+++ contracts/AdvancedBookstore.sol
@@ -5,5 +5,20 @@ pragma solidity ^0.8.26;
 import './Bookstore.sol';
 
 contract AdvancedBookstore is BookStore  {
+    mapping (uint256 => bool) public bestseller;
 
-}
+    event BookMarkedAsBestSeller(uint256 indexed bookId);
+
+    constructor (address _owner) BookStore() {}
+
+    function markBookAsBestSeller(uint256 bookId) public onlyOwner {
+        require(books[bookId].price != 0,"This Book Does Not Exist!");
+        bestseller[bookId] = true;
+        emit BookMarkedAsBestSeller(bookId);
+    }
+
+    function isBestSeller(uint256 bookId) public view returns (bool) {
+        return bestseller[bookId];
+    }
+
+ }
